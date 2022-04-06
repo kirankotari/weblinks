0c4b041345018b86125b09666b07fc0aaef3786c
Kiran Kumar Kotari <kirankotari@live.com>
2022-04-06 06:06:37 -0400

    lib. updated with stable code, without tests

--- README.md
+++ README.md
@@ -1,42 +1,49 @@
-# <package-name>
+# Weblinks
 
-## <add-badges>
+[![License: Apache 2.0](https://img.shields.io/badge/License-Apache2-yellow.svg)](https://opensource.org/licenses/Apache-2.0)
+[![Version: 1.1](https://img.shields.io/badge/Version-1.1-parrotgreen.svg)](https://github.com/kirankotari/weblinks)
+[![Downloads](https://pepy.tech/badge/weblinks)](https://pepy.tech/project/weblinks)
+[![Downloads](https://pepy.tech/badge/weblinks/week)](https://pepy.tech/project/weblinks/week)
 
 - [Introduction](#introduction)
-- [Docs](#docs)
-- [Commands](#commands)
-- [Pre-requisites](#pre-requisites)
 - [Installation and Downloads](#installation-and-downloads)
-- [FAQ](#faq)
-- [Other Resources](#other-resources)
-- [Bug Tracker and Support](#bug-tracker-and-support)
-- [Unit-Tests](#unit-tests)
+- [Usage](#usage)
 - [License and Copyright](#license-and-copyright)
 - [Author and Thanks](#author-and-thanks)
 
 ## Introduction
 
-## Docs
-
-## Commands
-
-## Pre-requisites
+Wanted to fetch the links from web and filter them using substring, a file extentions
 
 ## Installation and Downloads
 
-## FAQ
+`pip install weblinks`
 
-- **Question:** <question>?  
- **Answer:** <answer>.
+## Usage
 
-## Other Resources
+```
+usage: weblinks.py [-h] -w WEB [-u USERNAME] [-p PASSWORD] [-e EXT] [-d] [-v]
+                   substring
 
-## Bug Tracker and Support
+positional arguments:
+  substring             the sub-string in the links
 
-## Unit Tests
+optional arguments:
+  -h, --help            show this help message and exit
+  -w WEB, --web WEB     the website
+  -u USERNAME, --username USERNAME
+                        web login username
+  -p PASSWORD, --password PASSWORD
+                        web login password
+  -e EXT, --ext EXT     file extention
+  -d, --download        download links
+  -v, --verbosity
+```
 
 ## License and Copyright
 
+- weblinks is licensed [Apache 2.0](https://opensource.org/licenses/Apache-2.0) 2022
+
 ## Author and Thanks
 
-<package-name> was developed by [<author-name>](<author-profile-url or github-profile-link>)
+Weblinks was developed by [Kiran Kumar Kotari](https://github.com/kirankotari)
--- setup.py
+++ setup.py
@@ -2,6 +2,8 @@ from setuptools import setup, find_packages
 from os import path
 from io import open
 
+version = "1.1.0"
+
 # setup file path
 here = path.abspath(path.dirname(__file__))
 reqs = []
@@ -9,69 +11,45 @@ reqs = []
 # reading README.md, change if needed
 with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
-# if no README.md exist then un-comment the below line
-# long_description = '''<add-your-description-here>'''
 
-# reading pre-requisits if any else comment the block
 with open(path.join(here, 'requirements.txt'), encoding='utf-8')as f:
     read_lines = f.readlines()
     reqs = [ each.strip() for each in read_lines]
 
 setup(
     name='weblinks',
-    version='1.0',
+    version=version,
     description="Wanted to fetch the links from web and filter them using substring, a file extentions",
     long_description = long_description,
     long_description_content_type = 'text/markdown',
-    url = '<package-url or github-url>',
-    author = '<author-name>',
-    author_email='<author-email>',
-    
+    url = 'https://github.com/kirankotari/weblinks',
+    author = 'Kiran Kumar Kotari',
+    author_email = 'kirankotari@live.com',
+
     # Un-comment to enable command line feature
     entry_points={
     	'console_scripts': [
-    		'weblinks = weblinks.weblinks:get'
+    		'weblinks = weblinks.weblinks:main'
     	],
     },
 
     install_requires=reqs,
     classifiers = [ 
         # Choose the classifiers at https://pypi.org/classifiers/
-
         # Python Package Development Status. 
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 5 - Production/Stable',
         # Most common <development-status> are: 
         # 3 - Alpha
         # 4 - Beta
         # 5 - Production/Stable
 
-        'Intended Audience :: <audience>',
-        # Most common <audience> are:
-        # Developers
-        # Education
-        # Manufacturing
-        # Science/Research
-
-        'Topic :: <package-for> :: <package-useage-at>',
-        # In general <package-for> "Software Development" and <package-useage-at> "Build Tools"
-
-        'License :: <license-approved-by> :: <license>', 
-        # In general <license-approved-by> OSI Approved and <license> MIT License
-
-        # Python version support all sub-version of 2 and 3
-        'Programming Language :: Python :: 2',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: Apache Software License', 
         'Programming Language :: Python :: 3',
-
-        # Un-comment below if your package is for a special purpose
-        # 'Framework :: <frame-work>',
-        # 'Natural Language :: <language>',
-        # 'Operating System :: <os-name>',
-        # 'Programming Language :: <programming-language>',
-        # 'Topic :: <topic>',
+        'Operating System :: OS Independent',
         ],
-    keywords = '<package-keywords-seperated-by-space>',
-    
-    # add folder-names which need to be ignored under exclude=[]
-    packages = find_packages(where='.', exclude=['tests', 'data']),
+    keywords = 'weblinks getlinks web-links get-links fetch-links',
+    packages = find_packages(where='.', exclude=['tests']),
     include_package_data=True,
 )
